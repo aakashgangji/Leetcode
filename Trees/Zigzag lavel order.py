afb7d23f40f7dff99eb8e463d2eddf663cdd1195
aakashgangji <gangjiaakash@gmail.com>
2025-08-15 14:20:57 -0700

    ZigZZag

--- Trees/Zigzag lavel order.py
+++ Trees/Zigzag lavel order.py
@@ -9,15 +9,16 @@ Input: root = []
 Output: []
 
 '''
-
+from collections import deque
+from typing import List
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def zigzagLevelOrder(self, root) -> List[List[int]]:
         if not root:
             return []
 
@@ -56,3 +57,6 @@ class Solution:
                     zig=True
             zig=False
         return res
+
+root=TreeNode(3,TreeNode(4,TreeNode(5,TreeNode(7))))
+
