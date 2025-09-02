1a9f7d771c74608b126506acdcfd4bd6717bb505
aakashgangji <gangjiaakash@gmail.com>
2025-09-02 14:26:44 -0700

    Bt2

---
+++ Trees/PreorderTraversal.py
@@ -0,0 +1,21 @@
+"""
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res=[]
+        def dfs(root):
+            if not root:
+                return None
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return res
