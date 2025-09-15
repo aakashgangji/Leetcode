de54a0bc2cd1bda7887ae2aebf05091f8af9e1b7
aakashgangji <gangjiaakash@gmail.com>
2025-09-15 13:56:40 -0700

    MaxNumber

---
+++ General/MaxNumberofwords.py
@@ -0,0 +1,25 @@
+"""
+There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+Example 1:
+Input: text = "hello world", brokenLetters = "ad"
+Output: 1
+Explanation: We cannot type "world" because the 'd' key is broken.
+Example 2:
+Input: text = "leet code", brokenLetters = "lt"
+Output: 1
+Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+Example 3:
+Input: text = "leet code", brokenLetters = "e"
+Output: 0
+Explanation: We cannot type either word because the 'e' key is broken.
+"""
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        words=text.split()
+        count=0
+        b=set(brokenLetters)
+        for w in words:
+            if not any(letter in b for letter in w):
+                count+=1
+        return count
