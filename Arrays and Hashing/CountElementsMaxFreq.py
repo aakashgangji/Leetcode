79897ac38a723fe5d2a648df642948e552343fa6
aakashgangji <gangjiaakash@gmail.com>
2025-09-22 13:10:33 -0700

    Questions

--- Arrays and Hashing/CountElementsMaxFreq.py
+++ Arrays and Hashing/CountElementsMaxFreq.py
@@ -1,3 +1,18 @@
+"""
+You are given an array nums consisting of positive integers.
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+The frequency of an element is the number of occurrences of that element in the array.
+Example 1:
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+Example 2:
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+"""
 from collections import Counter
 
 def total_frequency_of_max_frequency_elements(nums):
@@ -11,5 +26,5 @@ def total_frequency_of_max_frequency_elements(nums):
 nums1 = [1, 2, 2, 3, 1, 4]
 nums2 = [1, 2, 3, 4, 5]
 
-print(total_frequency_of_max_frequency_elements(nums1))  # Output: 4
-print(total_frequency_of_max_frequency_elements(nums2))  # Output: 5
+print(total_frequency_of_max_frequency_elements(nums1))  
+print(total_frequency_of_max_frequency_elements(nums2)) 
