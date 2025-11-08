350d8cd49023dffef52a4c31931b4886d77f2b72
aakashgangji <gangjiaakash@gmail.com>
2025-11-08 16:37:05 -0700

    Answer

--- Mix/PascalsTriangle.py
+++ Mix/PascalsTriangle.py
@@ -17,4 +17,7 @@ class Solution:
                 row[j]=triangle[i-1][j-1]+triangle[i-1][j]
             triangle.append(row)
         return triangle
-    
+
+sol=Solution()
+print(sol.generate(5))  # Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+print(sol.generate(1))  # Output: [[1]]
