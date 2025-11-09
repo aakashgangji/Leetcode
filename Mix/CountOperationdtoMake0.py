a0bb64e20d0f54e980888bd00b2c2749c3cefe8f
aakashgangji <gangjiaakash@gmail.com>
2025-11-09 15:35:18 -0700

    CountOperations

--- Mix/CountOperationdtoMake0.py
+++ Mix/CountOperationdtoMake0.py
@@ -1,15 +1,9 @@
 """
 You are given two non-negative integers num1 and num2.
-
 In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
-
 For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
 Return the number of operations required to make either num1 = 0 or num2 = 0.
-
- 
-
 Example 1:
-
 Input: num1 = 2, num2 = 3
 Output: 3
 Explanation: 
@@ -19,7 +13,6 @@ Explanation:
 Now num1 = 0 and num2 = 1. Since num1 == 0, we do not need to perform any further operations.
 So the total number of operations required is 3.
 Example 2:
-
 Input: num1 = 10, num2 = 10
 Output: 1
 Explanation: 
---
+++ Mix/MinDistBw3EqElem.py
@@ -0,0 +1,42 @@
+"""
+Minimum Distance Between Three Equal Elements I
+You are given an integer array nums.
+A tuple (i, j, k) of 3 distinct indices is good if nums[i] == nums[j] == nums[k].
+The distance of a good tuple is abs(i - j) + abs(j - k) + abs(k - i), where abs(x) denotes the absolute value of x.
+Return an integer denoting the minimum possible distance of a good tuple. If no good tuples exist, return -1.
+Example 1:
+Input: nums = [1,2,1,1,3]
+Output: 6
+Explanation:
+The minimum distance is achieved by the good tuple (0, 2, 3).
+(0, 2, 3) is a good tuple because nums[0] == nums[2] == nums[3] == 1. Its distance is abs(0 - 2) + abs(2 - 3) + abs(3 - 0) = 2 + 1 + 3 = 6.
+Example 2:
+Input: nums = [1,1,2,3,2,1,2]
+Output: 8
+Explanation:
+The minimum distance is achieved by the good tuple (2, 4, 6).
+(2, 4, 6) is a good tuple because nums[2] == nums[4] == nums[6] == 2. Its distance is abs(2 - 4) + abs(4 - 6) + abs(6 - 2) = 2 + 2 + 4 = 8.
+Example 3:
+Input: nums = [1]
+Output: -1
+Explanation:
+There are no good tuples. Therefore, the answer is -1.
+"""
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        index_map=defaultdict(list)
+        for i,num in enumerate(nums):
+            index_map[num].append(i)
+        
+        min_distance=float('inf')
+        for indices in index_map.values():
+            if len(indices)>=3:
+                n=len(indices)
+                for i in range(n-2):
+                    j=i+1
+                    k=i+2
+                    dist=abs(indices[i]-indices[j])+abs(indices[j]-indices[k])+abs(indices[k]-indices[i])
+                    min_distance=min(min_distance,dist)
+        
+        return min_distance if min_distance!=float('inf') else -1
+        
