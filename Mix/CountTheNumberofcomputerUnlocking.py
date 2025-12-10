52e9c253cc0efd355f6007159cdcb581e0da0b75
aakashgangji <gangjiaakash@gmail.com>
2025-12-10 14:20:42 -0700

    Count

---
+++ Mix/CountTheNumberofcomputerUnlocking.py
@@ -0,0 +1,12 @@
+"""
+"""
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        MOD = 10**9 + 7
+        for i in range(1, len(complexity)):
+            if complexity[i] <= complexity[0]:
+                return 0
+        result = 1
+        for i in range(1, len(complexity)):
+            result = (result * i) % MOD
+        return result
