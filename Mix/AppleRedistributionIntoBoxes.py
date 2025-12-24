22165291b2c822e2d5a41cbe3c1d35d87c716e61
aakashgangji <gangjiaakash@gmail.com>
2025-12-24 14:19:44 -0700

    Apple

---
+++ Mix/AppleRedistributionIntoBoxes.py
@@ -0,0 +1,34 @@
+"""You are given an array apple of size n and an array capacity of size m.
+
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+Note that, apples from the same pack can be distributed into different boxes.
+
+ 
+
+Example 1:
+
+Input: apple = [1,3,2], capacity = [4,3,1,5,2]
+Output: 2
+Explanation: We will use boxes with capacities 4 and 5.
+It is possible to distribute the apples as the total capacity is greater than or equal to the total number of apples.
+Example 2:
+
+Input: apple = [5,5,5], capacity = [2,4,2,7]
+Output: 4
+Explanation: We will need to use all the boxes.
+"""
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        total_apples = sum(apple)
+        capacity.sort(reverse=True)
+        boxes_used = 0
+        for cap in capacity:
+            total_apples -= cap
+            boxes_used += 1
+            if total_apples <= 0:
+                return boxes_used
+        return boxes_used
+            
