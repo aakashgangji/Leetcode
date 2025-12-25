aa58064629fd6ed89ed69e2abcc5de82ca5be1fe
aakashgangji <gangjiaakash@gmail.com>
2025-12-25 11:02:00 -0700

    Solution

--- Mix/MaximizeHappinessOfChildren.py
+++ Mix/MaximizeHappinessOfChildren.py
@@ -44,3 +44,7 @@ class Solution:
       decremented += 1
 
     return ans
+s=Solution()
+print(s.maximumHappinessSum([1,2,3],2)) 
+print(s.maximumHappinessSum([1,1,1,1],2))
+print(s.maximumHappinessSum([2,3,4,5],1))   
