f85bdddb0488f61bf2637e9ddc8f29a38a0cdd79
aakashgangji <gangjiaakash@gmail.com>
2025-12-28 15:22:11 -0700

    CountNegative

---
+++ Arrays and Hashing/CountNegativeNumbers.py
@@ -0,0 +1,26 @@
+"""Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+"""
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        count = 0
+        row, col = 0, n - 1
+        while row < m and col >= 0:
+            if grid[row][col] < 0:
+                count += (m - row)
+                col -= 1
+            else:
+                row += 1
+        return count
