a0487a442e5059694ca045b1db166070995e43a8
aakashgangji <gangjiaakash@gmail.com>
2026-01-06 22:29:51 -0700

    Solution

--- Trees/MaxProductSplittedBT.py
+++ Trees/MaxProductSplittedBT.py
@@ -50,3 +50,13 @@ class Solution:
             max_product = max(max_product, product)
 
         return max_product % MOD
+s=Solution()
+# Example usage:
+# Construct the binary tree [1,2,3,4,5,6]
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+print(s.maxProduct(root))  # Output: 110    
