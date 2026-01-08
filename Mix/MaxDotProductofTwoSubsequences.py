5b5d53c63cf6dd84bc2daf17c2fd165494736076
aakashgangji <gangjiaakash@gmail.com>
2026-01-08 14:28:32 -0700

    Solution

---
+++ Mix/MaxDotProductofTwoSubsequences.py
@@ -0,0 +1,41 @@
+"""Given two arrays nums1 and nums2.
+
+Return the maximum dot product between non-empty subsequences of nums1 and nums2 with the same length.
+
+A subsequence of a array is a new array which is formed from the original array by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, [2,3,5] is a subsequence of [1,2,3,4,5] while [1,5,3] is not).
+
+ 
+
+Example 1:
+
+Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
+Output: 18
+Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from nums2.
+Their dot product is (2*3 + (-2)*(-6)) = 18.
+Example 2:
+
+Input: nums1 = [3,-2], nums2 = [2,-6,7]
+Output: 21
+Explanation: Take subsequence [3] from nums1 and subsequence [7] from nums2.
+Their dot product is (3*7) = 21.
+Example 3:
+
+Input: nums1 = [-1,-1], nums2 = [1,1]
+Output: -1
+Explanation: Take subsequence [-1] from nums1 and subsequence [1] from nums2.
+Their dot product is -1.
+"""
+class Solution:
+  def maxDotProduct(self, A: list[int], B: list[int]) -> int:
+    m = len(A)
+    n = len(B)
+    # dp[i][j] := the maximum dot product of the two subsequences nums[0..i)
+    # and nums2[0..j)
+    dp = [[-math.inf] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m):
+      for j in range(n):
+        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j],
+                               max(0, dp[i][j]) + A[i] * B[j])
+
+    return dp[m][n]
