05193347b32d086fc9cb8103d68aff492e9ffcf6
aakashgangji <gangjiaakash@gmail.com>
2026-01-15 09:42:01 -0700

    Solution

---
+++ Mix/MaxAreaOfSquareHoleInGrid.py
@@ -0,0 +1,62 @@
+"""You are given the two integers, n and m and two integer arrays, hBars and vBars. The grid has n + 2 horizontal and m + 2 vertical bars, creating 1 x 1 unit cells. The bars are indexed starting from 1.
+
+You can remove some of the bars in hBars from horizontal bars and some of the bars in vBars from vertical bars. Note that other bars are fixed and cannot be removed.
+
+Return an integer denoting the maximum area of a square-shaped hole in the grid, after removing some bars (possibly none).
+
+ 
+
+Example 1:
+
+
+
+Input: n = 2, m = 1, hBars = [2,3], vBars = [2]
+
+Output: 4
+
+Explanation:
+
+The left image shows the initial grid formed by the bars. The horizontal bars are [1,2,3,4], and the vertical bars are [1,2,3].
+
+One way to get the maximum square-shaped hole is by removing horizontal bar 2 and vertical bar 2.
+
+Example 2:
+
+
+
+Input: n = 1, m = 1, hBars = [2], vBars = [2]
+
+Output: 4
+
+Explanation:
+
+To get the maximum square-shaped hole, we remove horizontal bar 2 and vertical bar 2.
+
+Example 3:
+
+
+
+Input: n = 2, m = 3, hBars = [2,3], vBars = [2,4]
+
+Output: 4
+
+Explanation:
+
+One way to get the maximum square-shaped hole is by removing horizontal bar 3, and vertical bar 4.
+"""
+class Solution:
+    def maximizeSquareHoleArea(
+        self, n: int, m: int, hBars: List[int], vBars: List[int]
+    ) -> int:
+        def f(nums: List[int]) -> int:
+            nums.sort()
+            ans = cnt = 1
+            for i in range(1, len(nums)):
+                if nums[i] == nums[i - 1] + 1:
+                    cnt += 1
+                    ans = max(ans, cnt)
+                else:
+                    cnt = 1
+            return ans + 1
+
+        return min(f(hBars), f(vBars)) ** 2
