6e27f5e0d5b376d8535bae949d07e206bed9b135
aakashgangji <gangjiaakash@gmail.com>
2026-01-16 11:08:03 -0700

    Solution

---
+++ Mix/MaximumSquareAreabyRemovingFencesFromaField.py
@@ -0,0 +1,73 @@
+"""There is a large (m - 1) x (n - 1) rectangular field with corners at (1, 1) and (m, n) containing some horizontal and vertical fences given in arrays hFences and vFences respectively.
+
+Horizontal fences are from the coordinates (hFences[i], 1) to (hFences[i], n) and vertical fences are from the coordinates (1, vFences[i]) to (m, vFences[i]).
+
+Return the maximum area of a square field that can be formed by removing some fences (possibly none) or -1 if it is impossible to make a square field.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+Note: The field is surrounded by two horizontal fences from the coordinates (1, 1) to (1, n) and (m, 1) to (m, n) and two vertical fences from the coordinates (1, 1) to (m, 1) and (1, n) to (m, n). These fences cannot be removed.
+
+ 
+
+Example 1:
+
+
+
+Input: m = 4, n = 3, hFences = [2,3], vFences = [2]
+Output: 4
+Explanation: Removing the horizontal fence at 2 and the vertical fence at 2 will give a square field of area 4.
+Example 2:
+
+
+
+Input: m = 6, n = 7, hFences = [2], vFences = [4]
+Output: -1
+Explanation: It can be proved that there is no way to create a square field by removing fences.
+ here is a large (m - 1) x (n - 1) rectangular field with corners at (1, 1) and (m, n) containing some horizontal and vertical fences given in arrays hFences and vFences respectively.
+
+Horizontal fences are from the coordinates (hFences[i], 1) to (hFences[i], n) and vertical fences are from the coordinates (1, vFences[i]) to (m, vFences[i]).
+
+Return the maximum area of a square field that can be formed by removing some fences (possibly none) or -1 if it is impossible to make a square field.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+Note: The field is surrounded by two horizontal fences from the coordinates (1, 1) to (1, n) and (m, 1) to (m, n) and two vertical fences from the coordinates (1, 1) to (m, 1) and (1, n) to (m, n). These fences cannot be removed.
+
+ 
+
+Example 1:
+
+
+
+Input: m = 4, n = 3, hFences = [2,3], vFences = [2]
+Output: 4
+Explanation: Removing the horizontal fence at 2 and the vertical fence at 2 will give a square field of area 4.
+Example 2:
+
+
+
+Input: m = 6, n = 7, hFences = [2], vFences = [4]
+Output: -1
+Explanation: It can be proved that there is no way to create a square field by removing fences.
+ """
+class Solution:
+  def maximizeSquareArea(
+      self,
+      m: int,
+      n: int,
+      hFences: list[int],
+      vFences: list[int],
+  ) -> int:
+    hFences = sorted(hFences + [1, m])
+    vFences = sorted(vFences + [1, n])
+    hGaps = {hFences[i] - hFences[j]
+             for i in range(len(hFences))
+             for j in range(i)}
+    vGaps = {vFences[i] - vFences[j]
+             for i in range(len(vFences))
+             for j in range(i)}
+    maxGap = next((hGap
+                  for hGap in sorted(hGaps, reverse=True)
+                  if hGap in vGaps), -1)
+    return -1 if maxGap == -1 else maxGap**2 % (10**9 + 7)
