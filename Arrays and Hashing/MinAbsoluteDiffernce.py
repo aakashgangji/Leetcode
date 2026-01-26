aa323bb06d3b6dcdf62a691d33a153122ccc5c0e
aakashgangji <gangjiaakash@gmail.com>
2026-01-26 10:17:08 -0500

    Solution

--- Arrays and Hashing/MinAbsoluteDiffernce.py
+++ Arrays and Hashing/MinAbsoluteDiffernce.py
@@ -38,3 +38,10 @@ class Solution:
                 result.append([arr[i - 1], arr[i]])
         
         return result
+
+#or
+# class Solution:
+#     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+#         arr.sort()
+#         mi = min(b - a for a, b in pairwise(arr))
+#         return [[a, b] for a, b in pairwise(arr) if b - a == mi]
