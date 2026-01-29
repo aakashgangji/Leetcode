deb6cef7662c161f8993228f2c23598ef57eedf4
aakashgangji <gangjiaakash@gmail.com>
2026-01-29 09:24:57 -0500

    Solution

--- Mix/MinCostToConvertString1.py
+++ Mix/MinCostToConvertString1.py
@@ -65,3 +65,7 @@ class Solution:
       ans += dist[u][v]
 
     return ans
+s=Solution()
+print(s.minimumCost("abcd", "acbe", ["a","b","c","c","e","d"], ["b","c","b","e","b","e"], [2,5,5,1,2,20]))  #28
+print(s.minimumCost("aaaa", "bbbb", ["a","c"], ["c","b"], [1,2]))  #12
+print(s.minimumCost("abcd", "abce", ["a"], ["e"], [10000]))  #-1    
