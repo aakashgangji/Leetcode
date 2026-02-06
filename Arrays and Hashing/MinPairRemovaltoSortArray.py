8ed01a651d531e9d9860802297de729ef8b1f4de
aakashgangji <gangjiaakash@gmail.com>
2026-02-06 08:23:24 -0500

    Solution

--- Arrays and Hashing/MinPairRemovaltoSortArray.py
+++ Arrays and Hashing/MinPairRemovaltoSortArray.py
@@ -1,14 +1,4 @@
 """
-Code
-Testcase
-Testcase
-Test Result
-3507. Minimum Pair Removal to Sort Array I
-Easy
-Topics
-premium lock icon
-Companies
-Hint
 Given an array nums, you can perform the following operation any number of times:
 
 Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
@@ -16,8 +6,6 @@ Replace the pair with their sum.
 Return the minimum number of operations needed to make the array non-decreasing.
 
 An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
-
- 
 
 Example 1:
 
