ba41ce0fcf0e0f26a49fa9d72f2eaf25a81fa8db
aakashgangji <gangjiaakash@gmail.com>
2026-02-08 16:56:53 -0500

    Solution

--- Trees/Balanced Binary tree.py
+++ Trees/Balanced Binary tree.py
@@ -28,5 +28,26 @@ class Solution:
             balanced=(left[0] and right[0] and (abs(left[1]-right[1])<=1))
             return balanced,1+max(left[1],right[1])
         return dfs(root)[0]
+#or
+"""class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return 0
+            left=dfs(root.left)
+            if left==-1:
+                return -1
+            right=dfs(root.right)
+            if right==-1:
+                return -1
+            if abs(left-right)>1:
+                return -1
+            return 1+max(left,right)
+        return dfs(root)!=-1
+"""
+s=Solution()
+print(s.isBalanced([3,9,20,None,None,15,7]))  # True
+print(s.isBalanced([1,2,2,3,3,None,None,4,4]))  # False
+print(s.isBalanced([]))  # True 
     
     
