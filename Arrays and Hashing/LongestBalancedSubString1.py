11ad7b032155b232f1ee3113510a0f90c2c4faf8
aakashgangji <gangjiaakash@gmail.com>
2026-02-12 09:59:11 -0500

    Solution

---
+++ Arrays and Hashing/LongestBalancedSubString1.py
@@ -0,0 +1,52 @@
+"""You are given a string s consisting of lowercase English letters.
+
+A substring of s is called balanced if all distinct characters in the substring appear the same number of times.
+
+Return the length of the longest balanced substring of s.
+
+ 
+
+Example 1:
+
+Input: s = "abbac"
+
+Output: 4
+
+Explanation:
+
+The longest balanced substring is "abba" because both distinct characters 'a' and 'b' each appear exactly 2 times.
+
+Example 2:
+
+Input: s = "zzabccy"
+
+Output: 4
+
+Explanation:
+
+The longest balanced substring is "zabc" because the distinct characters 'z', 'a', 'b', and 'c' each appear exactly 1 time.​​​​​​​
+
+Example 3:
+
+Input: s = "aba"
+
+Output: 2
+
+Explanation:
+
+​​​​​​​One of the longest balanced substrings is "ab" because both distinct characters 'a' and 'b' each appear exactly 1 time. Another longest balanced substring is "ba"."""
+
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+
+        for i in range(n):
+            count = {}
+            for j in range(i, n):
+                count[s[j]] = count.get(s[j], 0) + 1
+                values = list(count.values())
+                if len(set(values)) == 1:
+                    ans = max(ans, j - i + 1)
+
+        return ans
