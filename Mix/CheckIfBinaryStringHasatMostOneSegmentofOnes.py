e4bd2c38fd6e6dcf3c8de8799695590a49169be3
aakashgangji <gangjiaakash@gmail.com>
2026-03-06 12:14:13 -0500

    Solution

---
+++ Mix/CheckIfBinaryStringHasatMostOneSegmentofOnes.py
@@ -0,0 +1,16 @@
+"""Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+Example 2:
+
+Input: s = "110"
+Output: true"""
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        return '01' not in s
